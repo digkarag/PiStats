90443b312c7e122ab5441309fcd41a5a0eab1cb3
Dimitris Garagounis <di.garagounis@gmail.com>
2017-03-26 21:39:21 +0300

    Fixes on try-except function. Add Error400 except

--- temp.py
+++ temp.py
@@ -4,34 +4,11 @@ import time
 import glob
 import signal
 import RPi.GPIO as GPIO
-from ubidots import ApiClient
+import ubidots
 from requests.exceptions import ConnectionError
 
 #Sleep for autorun feature, waiting for OS loading time
 time.sleep(30)
-
-#Signal handler for Ctrl+C to close leds
-def signal_handler_Ctrl_C(signal, frame):
-    print(' You pressed Ctrl+C!')
-    #All Leds OFF
-    GPIO.output(27,GPIO.LOW)
-    GPIO.output(17,GPIO.LOW)
-    GPIO.output(22,GPIO.LOW)
-    sys.exit(0)
-
-#Signal handler for Ctrl+Z for LogFile indicator clear
-def signal_handler_Ctrl_Z(signal, frame):
-    print(' Logfile check: complete')
-    #Blue Led OFF
-    GPIO.output(23,GPIO.LOW)
-    #Buzzer
-    GPIO.output(25,GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(25,GPIO.LOW)
-
-signal.signal(signal.SIGINT, signal_handler_Ctrl_C)
-signal.signal(signal.SIGTSTP, signal_handler_Ctrl_Z)
-
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -64,9 +41,34 @@ def read_temp():
         return temp_c
 
 
+#Signal handler for Ctrl+C to close leds
+def signal_handler_Ctrl_C(signal, frame):
+    print(' You pressed Ctrl+C!')
+    #All Leds OFF
+    GPIO.output(27,GPIO.LOW)
+    GPIO.output(17,GPIO.LOW)
+    GPIO.output(22,GPIO.LOW)
+    GPIO.output(24,GPIO.LOW)
+    sys.exit(0)
+
+
+#Signal handler for Ctrl+Z for LogFile indicator clear
+def signal_handler_Ctrl_Z(signal, frame):
+    print(' Logfile check: complete')
+    #Blue Led OFF
+    GPIO.output(23,GPIO.LOW)
+    #Buzzer
+    GPIO.output(25,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(25,GPIO.LOW)
+
+signal.signal(signal.SIGINT, signal_handler_Ctrl_C)
+signal.signal(signal.SIGTSTP, signal_handler_Ctrl_Z)
+
+
 #Ubidots Connection and valiable creation
 try:
-    api = ApiClient(token="peXr4PTbhJl1A2AEJxHc0xQGQMxVcD")
+    api = ubidots.ApiClient(token="peXr4PTbhJl1A2AEJxHc0xQGQMxVcD")
     temp = api.get_variable("58cd1a6c76254225983f07a1")
 
 except ConnectionError as errorconn:
@@ -86,7 +88,7 @@ GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22,GPIO.OUT)
 GPIO.setup(23,GPIO.OUT)
 GPIO.setup(25,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT) #White led, not used yet
+GPIO.setup(24,GPIO.OUT)
 
 #Buzzer, single beep at program startup
 GPIO.output(25,GPIO.HIGH)
@@ -97,7 +99,7 @@ while True:
 
     #Green Led ON
     GPIO.output(17,GPIO.HIGH)
-    time.sleep(2)
+    time.sleep(0.5)
 
     #Read temperature from sensor
     deg_c = read_temp()
@@ -111,6 +113,8 @@ while True:
         GPIO.output(27,GPIO.LOW)
         #Yellow Led OFF
         GPIO.output(22,GPIO.LOW)
+        #White Led OFF
+        GPIO.output(24,GPIO.LOW)
 
     except  ConnectionError as ex:
         print ex
@@ -144,7 +148,7 @@ while True:
         GPIO.output(22,GPIO.HIGH)
         time.sleep(10)
 
-    except ubidots.apiclient.UbidotsError404 as ex2:
+    except ubidots.UbidotsError500 as ex2:
         print ex2
         sys.stdout.flush()
 
@@ -160,7 +164,7 @@ while True:
         GPIO.output(27,GPIO.HIGH)
         time.sleep(10)
 
-    except upidots.apiclient.UbidotsError500 as ex3:
+    except ubidots.UbidotsError404 as ex3:
         print ex3
         sys.stdout.flush()
 
@@ -172,9 +176,26 @@ while True:
 
         #Blue Led ON
         GPIO.output(23,GPIO.HIGH)
-        #Red Led ON
-        GPIO.output(27,GPIO.HIGH)
+        #White Led ON
+        GPIO.output(24,GPIO.HIGH)
         time.sleep(10)
+
+    except ubidots.UbidotsError400 as ex4:
+        print ex4
+        sys.stdout.flush()
+
+        #LogFile entry
+        file=open("logfile.txt","a")
+        file.write(time.strftime("%H:%M:%S") +" , "+time.strftime("%d/%m/%Y"))
+        file.write(str(ex4)+'\n')
+        file.close()
+
+        #Blue Led ON
+        GPIO.output(23,GPIO.HIGH)
+        #White Led ON
+        GPIO.output(24,GPIO.HIGH)
+        time.sleep(10)
+
 
     #Print functions
     print( time.strftime("%d/%m/%Y") + ", " + time.strftime("%H:%M:%S") + ", "
@@ -183,4 +204,4 @@ while True:
 
     #Green Led OFF
     GPIO.output(17,GPIO.LOW)
-    time.sleep(2)
+    time.sleep(0.5)
