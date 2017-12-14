5e9136c1e509c757c71119a9a00d7aae06bda8fe
digkarag <di.garagounis@gmail.com>
2017-12-14 23:00:39 +0200

    Update

---
+++ data.py
@@ -0,0 +1,4 @@
+#Data File for defined values and Web Data
+
+#posturl for url used in requests.post function
+posturl = "http://things.ubidots.com/api/v1.6/devices/ZeroW/?token=A1E-ZCaNbMJodCnxdT2GPU53PIDkUSwnma"
--- temp.py
+++ temp.py
@@ -1,56 +1,26 @@
 import os
 import sys
+import dht11
+import datetime
 import time
 import glob
 import signal
-import ubidots
 import RPi.GPIO as GPIO
-from requests.exceptions import ConnectionError
+import requests
 
-#Sleep for autorun feature, waiting for OS loading time
-time.sleep(30)
-
-#System files for sensor input
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
+from data import posturl
+url = posturl
 
 #GPIO Initialization
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.cleanup()
+
 GPIO.setup(17,GPIO.OUT) #Green Led: System Run indicator
 GPIO.setup(27,GPIO.OUT) #Red Led: Connection Error
 GPIO.setup(22,GPIO.OUT) #White Led: Logfile Entry
 GPIO.setup(23,GPIO.OUT) #Yellow Led: Ubidots Error
 GPIO.setup(25,GPIO.OUT) #Buzzer
-
-
-#Read from w1_slave file
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-#Extract and return temperature from w1_slave
-#Temp_f is optional value for fahrenheit degrees
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        temp_c = round(temp_c, 1)
-        return temp_c
-
 
 #Signal handler for Ctrl+C to close leds
 def signal_handler_Ctrl_C(signal, frame):
@@ -78,147 +48,20 @@ def signal_handler_Ctrl_Z(signal, frame):
 signal.signal(signal.SIGINT, signal_handler_Ctrl_C)
 signal.signal(signal.SIGTSTP, signal_handler_Ctrl_Z)
 
-
-#LogFile Function
-def logFile(error):
-    try:
-
-        file=open('logfile.txt',"a+")
-        file.write(time.strftime("%H:%M:%S") +" , "+time.strftime("%d/%m/%Y")+" , ")
-        file.write(str(error)+'\n')
-        file.close()
-
-        #White Led ON
-        GPIO.output(22,GPIO.HIGH)
-
-    except Exception as error:
-
-        #print error
-        sys.exit(0)
-
-
-#Ubidots Connection and valiable creation
-try:
-    api = ubidots.ApiClient(token="peXr4PTbhJl1A2AEJxHc0xQGQMxVcD")
-    temp = api.get_variable("58cd1a6c76254225983f07a1")
-
-    #Yellow LED OFF
-    GPIO.output(23,GPIO.LOW)
-
-except Exception as error:
-
-    #print error
-    #Yellow Led ON
-    GPIO.output(23,GPIO.HIGH)
-    time.sleep(10)
-
-    #LogFile entry
-    logFile(error)
-
-
-#Buzzer, single beep at program startup
-GPIO.output(25,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(25,GPIO.LOW)
-
+#Read data using pin 21 DHT11
+instance = dht11.DHT11(pin=21)
 
 while True:
-
-    #Green Led ON
-    GPIO.output(17,GPIO.HIGH)
-    time.sleep(2)
-
-    #Read temperature from sensor
-    deg_c = read_temp()
-
-    #Sending value to Ubidots via save_value function
-    try:
-
-        temp.save_value({"value": deg_c})
-
-        #Red Led OFF
-        GPIO.output(27,GPIO.LOW)
-        #Yellow Led OFF
-        GPIO.output(23,GPIO.LOW)
-
-    except ConnectionError as ex:
-        #print("Connection error"+"\n")
-        #print ex
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex)
-
-        #Red Led ON
-        GPIO.output(27,GPIO.HIGH)
-        time.sleep(10)
-
-    except ValueError as ex1:
-        #print("ValueError"+"\n")
-        #print ex1
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex1)
-
-        #Yellow Led ON
-        GPIO.output(23,GPIO.HIGH)
-        time.sleep(10)
-
-    except ubidots.UbidotsError500 as ex2:
-        #print("Ubidots Error 500"+"\n")
-        #print ex2
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex2)
-
-        #Yellow Led ON
-        GPIO.output(23,GPIO.HIGH)
-        time.sleep(10)
-
-    except ubidots.UbidotsError404 as ex3:
-        #print("Ubidots Error 404"+"\n")
-        #print ex3
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex3)
-
-        #Yellow Led ON
-        GPIO.output(23,GPIO.HIGH)
-        time.sleep(10)
-
-    except ubidots.UbidotsError400 as ex4:
-        #print("Ubidots Error 400"+"\n")
-        #print ex4
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex4)
-
-        #Yellow Led ON
-        GPIO.output(23,GPIO.HIGH)
-        time.sleep(10)
-
-    except Exception as ex5:
-        #print("General Exception"+"\n")
-        #print ex5
-        #sys.stdout.flush()
-
-        #LogFile entry
-        logFile(ex5)
-
-        #Red Led ON
-        GPIO.output(27,GPIO.HIGH)
-        time.sleep(10)
-
-
-    #Print functions
-    #print( time.strftime("%d/%m/%Y") + ", " + time.strftime("%H:%M:%S") + ", "
-    #       + 'Temperature in Celsius: ' + str(deg_c) + '\n')
-    #sys.stdout.flush()
-
-    #Green Led OFF
-    GPIO.output(17,GPIO.LOW)
-    time.sleep(2)
+   #Read Data from DHT11
+    result = instance.read()
+    if result.is_valid():
+        try:
+            humidity = result.humidity
+            payload ={'Humidity': humidity}
+            postresp = requests.post(url,data=payload)
+            print postresp
+        except Exception as ex:
+            time.sleep(5)
+        print result.humidity
+        time.sleep(1)
+    time.sleep(1)
